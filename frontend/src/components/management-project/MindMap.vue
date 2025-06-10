166bf2c7b9ba66934cefafa1d0f10397b14b6435
javad <kasiri.javad@gmail.com>
2025-06-10 23:28:27 +0400

    first commit

---
+++ frontend/src/components/management-project/MindMap.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>Mind Map</h3>
+  </div>
+</template>
---
+++ frontend/src/components/management/ManagementProject.vue
@@ -0,0 +1,7 @@
+<template>
+  <MindMap />
+</template>
+
+<script setup>
+import MindMap from '../management-project/MindMap.vue'
+</script>
